b23561991120a8ddc96529f156597760e064d989
Ethan Rose <ethanrose.er@googlemail.com>
2024-02-26 01:06:55 +0000

    enhancement: changed Register page and added css to app.scss

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,85 @@
 import React from "react";
 import "./Register.css";
+import "../../App.css";
+
+import { Link, NavLink } from "react-router-dom";
+
+// imported icons
+import { FaUserShield } from "react-icons/fa";
+import { BsFillShieldLockFill } from "react-icons/bs";
+import { AiOutlineSwapRight } from "react-icons/ai";
+import { MdMarkEmailRead } from "react-icons/md";
+
+// Import assets
+import video from "../../LoginAssets/bookVid.mp4";
+import logo from "../../LoginAssets/logo.png";
 
 const Register = () => {
   return (
-    <div>
-      This is Register Page
-      <a href="/">To Login</a>
+    <div className="registerPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
+
+          <div className="textDiv">
+            <h2 className="title">A book For Every Occasion</h2>
+            <p>Read at your convenience!</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">Have an account?</span>
+            <Link to={"/"}>
+              <button className="btn">Login</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="formDiv flex">
+          <div className="headerDiv">
+            <img src={logo} alt="Logo Image" />
+            <h3>A bit About Yourself</h3>
+          </div>
+
+          <form action="" className="form grid">
+            <div className="inputDiv">
+              <label htmlFor="email">Email</label>
+              <div className="input flex">
+                <MdMarkEmailRead className="icon" />
+                <input type="email" id="email" placeholder="Enter Email" />
+              </div>
+            </div>
+
+            <div className="inputDiv">
+              <label htmlFor="username">Username</label>
+              <div className="input flex">
+                <FaUserShield className="icon" />
+                <input type="text" id="username" placeholder="Enter Username" />
+              </div>
+            </div>
+
+            <div className="inputDiv">
+              <label htmlFor="password">Password</label>
+              <div className="input flex">
+                <BsFillShieldLockFill className="icon" />
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" className="btn flex">
+              <span>Register</span>
+              <AiOutlineSwapRight className="icon" />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot your password? <a href="">Click Here</a>
+            </span>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
